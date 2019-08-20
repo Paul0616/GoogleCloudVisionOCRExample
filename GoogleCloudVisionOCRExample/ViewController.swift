2521973fcc885e00a9cf60b924815a9e97837b66
Paul Oprea <omd.paul@gmail.com>
2019-08-20 13:03:12 +0300

    Initial Commit

---
+++ GoogleCloudVisionOCRExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleCloudVisionOCRExample
+//
+//  Created by Paul Oprea on 20/08/2019.
+//  Copyright © 2019 Paul Oprea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
