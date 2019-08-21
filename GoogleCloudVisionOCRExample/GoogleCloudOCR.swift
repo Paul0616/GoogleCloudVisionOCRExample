1ca330566e448b8e02c34a3153ea40f77ddddb6a
Paul Oprea <omd.paul@gmail.com>
2019-08-21 09:56:47 +0300

    sss

--- GoogleCloudVisionOCRExample/GoogleCloudOCR.swift
+++ GoogleCloudVisionOCRExample/GoogleCloudOCR.swift
@@ -10,7 +10,7 @@ import Foundation
 import Alamofire
 
 class GoogleCloudOCR {
-    private let apiKey = ""//"AIzaSyA137QmmGAd-Wi-hbt_SPIm4F1iPFNauGY"
+    private let apiKey = ""
     private var apiURL: URL {
         return URL(string: "https://vision.googleapis.com/v1/images:annotate?key=\(apiKey)")!
     }
